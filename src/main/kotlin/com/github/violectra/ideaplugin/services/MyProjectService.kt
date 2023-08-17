3f95e2f1c290783d3cc77909df3e6a1a1236b131
Valeria Golovina <valeria.golovina@gmail.com>
2023-08-17 16:39:41 +0300

    Tree with external subtrees as a text

--- src/main/kotlin/com/github/violectra/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/violectra/ideaplugin/services/MyProjectService.kt
@@ -10,7 +10,9 @@ import com.intellij.openapi.roots.CompilerModuleExtension
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlFile
 import com.intellij.util.xml.DomManager
 import java.io.IOException
@@ -22,7 +24,7 @@ import java.nio.file.Path
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
-    internal var window : MyToolWindow? = null
+    internal var window: MyToolWindow? = null
 
     init {
         thisLogger().info(MyBundle.message("projectService", project.name))
@@ -42,45 +44,82 @@ class MyProjectService(project: Project) {
     }
 
     private fun readDomXmlFile(project: Project, file: PsiFile): String {
-        val manager = DomManager.getDomManager(project)
-        val virtualFile = file
-        if (virtualFile is XmlFile) {
-            val root: Root = manager.getFileElement(virtualFile, Root::class.java)?.rootElement ?: throw RuntimeException("Root not found")
-            val writer = StringWriter()
-            PrintWriter(writer).use { printWriter ->
-                printNode(root, 0, printWriter)
-            }
-            return writer.toString()
+        val root: Root = getXmlRoot(file, project)
+        val rootPath = file.parent?.virtualFile?.toNioPath() ?: throw RuntimeException("Parent folder not found")
+        val name = file.name
+        val writer = StringWriter()
+        PrintWriter(writer).use { printWriter ->
+            printNode(root, 0, printWriter, rootPath, project, setOf(name))
         }
-        throw RuntimeException("File is not correct")
+        return writer.toString()
     }
 
-    private fun printNode(root: MyNode, indentLevel: Int, writer: PrintWriter) {
+    private fun findFile(path: Path, project: Project): PsiFile {
+        val file =
+            VirtualFileManager.getInstance().findFileByNioPath(path) ?: throw RuntimeException("No internal file found")
+        return PsiManager.getInstance(project).findFile(file) ?: throw RuntimeException("No internal file found")
+    }
+
+    private fun getXmlRoot(
+        file: PsiFile,
+        project: Project
+    ) = if (file is XmlFile) {
+        DomManager.getDomManager(project).getFileElement(file, Root::class.java)?.rootElement
+            ?: throw RuntimeException("Root not found")
+    } else throw RuntimeException("File type is not XML")
+
+
+    private fun printNode(
+        root: MyNode,
+        indentLevel: Int,
+        writer: PrintWriter,
+        rootPath: Path,
+        project: Project,
+        usedSrc: Set<String>
+    ) {
         val tag = root.xmlTag?.name
         val indent = " ".repeat(indentLevel)
-        writer.println(
-            when (root) {
-                is Root -> "$indent$tag"
-                is NodeRef -> {
-                    val title = root.getTitle().value ?: root.getValue()
-                    val srcFileName = root.getSrc().value
-                    val info = "${root.getId().value}, $srcFileName"
-                    "$indent$tag[$info] $title"
-                }
-                is MyNodeWithIdAttribute -> {
-                    val title = root.getTitle().value ?: root.getValue()
-                    val info = root.getId().value
-                    "$indent$tag[$info] $title"
-                }
-                else -> ""
-            }
-        )
+        writer.println(nodeString(root, indent, tag))
         if (root is MyNodeWithChildren) {
             for (child in root.getSubNodes()) {
-                printNode(child, indentLevel + 1, writer)
+                printNode(child, indentLevel + 1, writer, rootPath, project, usedSrc)
+            }
+        } else if (root is NodeRef) {
+            val srcFileName = root.getSrc().value ?: throw RuntimeException("No src for ref")
+            val path = rootPath.resolve(srcFileName)
+            val file = findFile(path, project)
+            if (file.name !in usedSrc) {
+                val externalRoot: Root = getXmlRoot(file, project)
+                for (child in externalRoot.getSubNodes()) {
+                    printNode(child, indentLevel + 1, writer, rootPath, project, usedSrc + file.name)
+                }
+            } else {
+                writer.println("$indent ...");
             }
         }
     }
+
+    private fun nodeString(
+        root: MyNode,
+        indent: String,
+        tag: String?
+    ) = when (root) {
+        is Root -> "$indent$tag"
+        is NodeRef -> {
+            val srcFileName = root.getSrc().value
+            val id = root.getId().value
+            "$indent$tag[$id, $srcFileName] ${getTitle(root)}"
+        }
+
+        is MyNodeWithIdAttribute -> {
+            val id = root.getId().value
+            "$indent$tag[$id] ${getTitle(root)}"
+        }
+
+        else -> ""
+    }
+
+    private fun getTitle(root: MyNodeWithIdAttribute) = root.getTitle().value ?: root.getValue()
 
     private fun getTextFromCorrespondingFile(project: Project, file: PsiFile): String {
         val index: ProjectFileIndex = ProjectFileIndex.getInstance(project)
